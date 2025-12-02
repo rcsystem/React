20655fbbc963eb551710e8b1af700ed3329776a1
Rafael Cruz <rafael.cruz.aguilar1@gmail.com>
2025-12-02 17:16:57 -0600

    capitulo de timeout para input buscar

--- 03-gifs-app/src/shared/components/SearchBar.tsx
+++ 03-gifs-app/src/shared/components/SearchBar.tsx
@@ -1,43 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
+  placeholder: string;
 
-    placeholder: string;
-
-    onQuery?: (query: string) => void;
+  onQuery?: (query: string) => void;
 }
 
+export const SearchBar = ({ placeholder = "Buscar", onQuery }: Props) => {
+  const [query, setQuery] = useState("");
 
+  useEffect(() => {
+    const timeoutID = setTimeout(() => {
+      if (query.trim().length === 0) return;
+      onQuery?.(query);
+    }, 700);
 
-export const SearchBar = ({placeholder='Buscar', onQuery }:Props) => {
-
-  const [query, setQuery] = useState('');
+    return () => clearTimeout(timeoutID);
+  }, [query, onQuery]);
 
   const handleSearch = () => {
-        onQuery?.(query);
-        setQuery('');
-  }
+    onQuery?.(query);
+    setQuery("");
+  };
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-      if(event.key === 'Enter'){
-        handleSearch();
-      }
+    if (event.key === "Enter") {
+      handleSearch();
     }
+  };
 
   return (
-     <div className="search-container">
-      <input 
-      type="text" 
-      placeholder={placeholder} 
-      value={query}
-      onChange ={(event) => {
-        const newQuery = event.target.value;
-        setQuery(newQuery);
-      }}
-      onKeyDown={handleKeyDown}
+    <div className="search-container">
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={query}
+        onChange={(event) => {
+          const newQuery = event.target.value;
+          setQuery(newQuery);
+        }}
+        onKeyDown={handleKeyDown}
       />
       <button onClick={handleSearch}>Buscar</button>
     </div>
-
-  )
-}
+  );
+};
